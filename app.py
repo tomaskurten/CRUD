e953b91a5baa88cb4085ea92a178670f859b8098
tomaskurten <tomas.kurten@bue.edu.ar>
2023-12-01 16:46:26 -0300

    sec

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request,redirect
 from flaskext.mysql import MySQL
-
+from datetime import datetime 
 app=Flask(__name__)
 
 mysql = MySQL()
@@ -12,7 +12,14 @@ mysql.init_app(app)
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    sql = "SELECT * FROM `sistema`.`empleados`;"
+    conn=mysql.connect()
+    cursor=conn.cursor()
+    cursor.execute(sql)
+    empleados=cursor.fetchall()
+    # print(empleados)
+    conn.commit()
+    return render_template("index.html",empleados=empleados)
 
 @app.route("/create")
 def create():
@@ -21,17 +28,67 @@ def create():
 
 @app.route("/store",methods=["POST"])
 def storage():
-    nombre=request.form["txtNombre"]
-    correo=request.form["txtCorreo"]
-    datos=(nombre,correo)
-    # foto=request.files["txtFoto"]
-    sql="INSERT INTO `sistema`.`empleados` (`id`, `nombre`, `correo`, `foto`) VALUES (NULL, %s, %s, 'foto4.jpg');"
+    _nombre=request.form['txtNombre']
+    _correo=request.form['txtCorreo']
+    _foto=request.files['txtFoto']
+    now= datetime.now()
+    tiempo= now.strftime("%Y%H%M%S")
+    if _foto.filename!='':
+        nuevoNombreFoto=tiempo+_foto.filename
+        _foto.save("uploads/"+nuevoNombreFoto)
+
+    sql = "INSERT INTO `sistema`.`empleados` (`id`, `nombre`, `correo`, `foto`) VALUES (NULL, %s, %s, %s);"
+    datos=(_nombre,_correo,nuevoNombreFoto)
+
     conn=mysql.connect()
     cursor=conn.cursor()
     cursor.execute(sql,datos)
     conn.commit()
     return render_template("index.html")
     
+
+@app.route("/destroy/<int:id>")
+def destroy(id):
+    conn=mysql.connect()
+    cursor=conn.cursor()
+    sql="DELETE FROM `sistema`.`empleados` WHERE id=%s"
+    cursor.execute(sql,id)
+    conn.commit()
+    return redirect("/")
+
+
+@app.route("/edit/<int:id>")
+def edit(id):
+    sql = "SELECT * FROM `sistema`.`empleados` WHERE id=%s;"
+    conn=mysql.connect()
+    cursor=conn.cursor()
+    cursor.execute(sql,id)
+    empleados=cursor.fetchall()
+    conn.commit()
+    
+    # print(empleados)
+    return render_template("edit.html",empleados=empleados)
+
+
+
+
+
+@app.route('/update', methods=['POST'])
+def update():
+    _nombre=request.form['txtNombre']
+    _correo=request.form['txtCorreo']
+    # _foto=request.files['txtFoto']
+    id=request.form['txtID']
+
+    datos=(_nombre,_correo,id)
+    conn = mysql.connect()
+    cursor = conn.cursor()
+    sql = "UPDATE `sistema`.`empleados` SET `nombre`=%s, `correo`=%s WHERE id=%s;"
+    cursor.execute(sql,datos)
+    conn.commit()
+    return redirect('/')
+
+
 
 if __name__=="__main__":
     app.run(debug=True)
